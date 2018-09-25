cd9c9fba7a2d52dea3ba86ec26997523adbabb62
darlok77 <g.dalleau77@hotmail.com>
2018-09-25 12:16:16 +0200

    Update index.js
    hide api key

--- index.js
+++ index.js
@@ -15,14 +15,14 @@ module.exports = class BotMeteo {
       'url': 'https://api.openweathermap.org/data/2.5/weather',
       'qs': {
         'q': this.name,
-        'APPID': 'a4ead3f50eaaa932f599d8ee4fc963b9',
+        'APPID': 'your-APPID',//change
         'lang': 'fr',
         'units': 'metric'
       },
       'headers': {
         'accept': 'application/json',
-        'x-ibm-client-secret': 'C1yD0eY7sT2yL8sJ4yR4tX5fW7eP7tV1dC6qA7fX4aU1gQ8oX8',
-        'x-ibm-client-id': '98cc4890-9c5b-4a9c-b2d9-3c9fdf7c4c18'
+        'x-ibm-client-secret': 'your-client-secret',//change
+        'x-ibm-client-id': 'your-client-id'//change
       }
     };
 
